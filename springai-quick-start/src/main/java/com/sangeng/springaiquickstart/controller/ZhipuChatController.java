2de2ef59204d37091d8a48954c115e617a57eb0d
Administrator <admin@example.com>
2026-02-02 23:37:56 +0800

    E:\Develop\AI\sangeng-springai-examples\springai-quick-start\.gitattributes E:\Develop\AI\sangeng-springai-examples\.gitattributes E:\Develop\AI\sangeng-springai-examples\springai-quick-start\.gitignore E:\Develop\AI\sangeng-springai-examples\.gitignore E:\Develop\AI\sangeng-springai-examples\src\main\resources\application.properties E:\Develop\AI\sangeng-springai-examples\springai-quick-start\src\main\resources\application.yml E:\Develop\AI\sangeng-springai-examples\quick_start2\src\main\java\com\sangeng\Main.java E:\Develop\AI\sangeng-springai-examples\.mvn\wrapper\maven-wrapper.properties E:\Develop\AI\sangeng-springai-examples\springai-quick-start\.mvn\wrapper\maven-wrapper.properties E:\Develop\AI\sangeng-springai-examples\springai-quick-start\mvnw E:\Develop\AI\sangeng-springai-examples\mvnw E:\Develop\AI\sangeng-springai-examples\springai-quick-start\mvnw.cmd E:\Develop\AI\sangeng-springai-examples\mvnw.cmd E:\Develop\AI\sangeng-springai-examples\quick_start2\pom.xml E:\Develop\AI\sangeng-springai-examples\springai-quick-start\pom.xml E:\Develop\AI\sangeng-springai-examples\pom.xml E:\Develop\AI\sangeng-springai-examples\src\main\java\com\sangeng\sangengspringaiexamples\SangengSpringaiExamplesApplication.java E:\Develop\AI\sangeng-springai-examples\src\test\java\com\sangeng\sangengspringaiexamples\SangengSpringaiExamplesApplicationTests.java E:\Develop\AI\sangeng-springai-examples\springai-quick-start\src\main\java\com\sangeng\springaiquickstart\SpringaiQuickStartApplication.java E:\Develop\AI\sangeng-springai-examples\springai-quick-start\src\test\java\com\sangeng\springaiquickstart\SpringaiQuickStartApplicationTests.java E:\Develop\AI\sangeng-springai-examples\springai-quick-start\src\main\java\com\sangeng\springaiquickstart\controller\ZhipuChatController.java

--- springai-quick-start/src/main/java/com/sangeng/springaiquickstart/controller/ZhipuChatController.java
+++ springai-quick-start/src/main/java/com/sangeng/springaiquickstart/controller/ZhipuChatController.java
@@ -4,10 +4,15 @@ import org.springframework.ai.chat.messages.SystemMessage;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatModel;
 
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.zhipuai.ZhiPuAiChatOptions;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/zhipu")
@@ -38,7 +43,34 @@ public class ZhipuChatController {
         UserMessage userMessage=new UserMessage(query);
         // invoke model
         String call = chatModel.call(systemMessage, userMessage);
-        return call;
+    return call;
     }
+
+    @GetMapping("/changeOptions")
+    public ChatResponse chatOptions(@RequestParam(name = "query") String query){
+        SystemMessage systemMessage = new SystemMessage("you are a helpful assistant.");
+        UserMessage userMessage = new UserMessage(query);
+        ZhiPuAiChatOptions zhiPuAiChatOptions = new ZhiPuAiChatOptions();
+        zhiPuAiChatOptions.setModel("glm-4.5");
+        zhiPuAiChatOptions.setMaxTokens(15536);
+        zhiPuAiChatOptions.setTemperature(0.0);
+        Prompt prompt = new Prompt(List.of(systemMessage, userMessage), zhiPuAiChatOptions);
+        return chatModel.call(prompt);
+
+    }
+    @GetMapping("/chatResponse")
+    public String chatResponse(@RequestParam(name = "query") String query){
+        SystemMessage systemMessage = new SystemMessage("you are a helpful assistant.");
+        UserMessage userMessage = new UserMessage(query);
+        ZhiPuAiChatOptions zhiPuAiChatOptions = new ZhiPuAiChatOptions();
+        zhiPuAiChatOptions.setModel("glm-4.5");
+        zhiPuAiChatOptions.setMaxTokens(15536);
+        zhiPuAiChatOptions.setTemperature(0.0);
+        Prompt prompt = new Prompt(List.of(systemMessage, userMessage), zhiPuAiChatOptions);
+        ChatResponse chatResponse = chatModel.call(prompt);
+        return chatResponse.getResult().getOutput().getText();
+
+    }
+
 }
 
